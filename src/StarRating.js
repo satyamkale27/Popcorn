fdcc32b5ace08f9adf768cbb0536309c00a381e4
satyam <satyamkale271@gmail.com>
2024-07-20 16:04:54 +0530

    Array.from method used for stars

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,3 +1,12 @@
 export default function StarRating() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <div>
+        {Array.from({ length: 5 }, (_, i) => (
+          <span>s{i + 1}</span>
+        ))}
+      </div>
+      <p>10</p>
+    </div>
+  );
 }
