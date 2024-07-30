1f42a0e1494137b002a0ff0fca25449c6896c6aa
satyam <satyamkale271@gmail.com>
2024-07-30 18:41:20 +0530

    used async and await function

--- src/App.js
+++ src/App.js
@@ -95,11 +95,16 @@ function Main({ children }) {
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
-
+  const query = "interstellar";
   useEffect(function () {
-    fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=interstellar`)
-      .then((res) => res.json())
-      .then((data) => setMovies(data.Search));
+    async function fetchMovies() {
+      const res = await fetch(
+        `https://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${query}`
+      );
+      const data = await res.json();
+      setMovies(data.Search);
+    }
+    fetchMovies();
   }, []);
 
   return (
