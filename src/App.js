1ca46c8c62dd341054014911315582f572111dca
satyam <satyamkale271@gmail.com>
2024-07-31 22:19:23 +0530

    hover styling

--- src/App.js
+++ src/App.js
@@ -212,7 +212,7 @@ function Box({ children }) {
 }
 function MovieList({ movies, onSelectMovie }) {
   return (
-    <ul className="list">
+    <ul className="list list-movies">
       {movies?.map((movie) => (
         <Movie movie={movie} key={movie.imdbID} onSelectMovie={onSelectMovie} />
       ))}
