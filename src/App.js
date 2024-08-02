7c4d6db2b066d0c46d947e0040ffc828e40dcc82
satyam <satyamkale271@gmail.com>
2024-08-02 21:31:49 +0530

    clearn up effect

--- src/App.js
+++ src/App.js
@@ -329,6 +329,9 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     function () {
       if (!title) return;
       document.title = `Movie | ${title}`;
+      return function () {
+        document.title = "Use popcorn";
+      };
     },
     [title]
   );
